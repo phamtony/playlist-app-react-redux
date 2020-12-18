c655b055a54d134aa0876d878e61164b52d7b778
Tony Pham <tony.p@lashowroom.com>
2020-12-18 13:31:46 -0800

    wired up provider, now the app can make use of the connect

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import {combineReducers} from "redux";
+
 const songsReducer = () => {
   return [
       {
@@ -26,3 +28,8 @@ const selectedSongReducer = (selectedSong=null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+   songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
